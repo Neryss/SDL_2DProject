1a4cf62d6a918516da557c314bcab3b0e1df6b73
neryss <kuroysiris@gmail.com>
2020-07-26 00:33:48 +0200

    Init

---
+++ SDL_project/src/main.cpp
@@ -0,0 +1,30 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include "RenderWindow.hpp"
+
+int		main(int argc, char **argv)
+{
+	if(SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+	if(!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG init HAS FAILED. Error: " << SDL_GetError() << std::endl;
+	RenderWindow window("Game v1.0", 1280, 720);
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while(gameRunning)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+	}
+	window.cleanUp();
+	SDL_Quit();
+
+	return(0);
+}
